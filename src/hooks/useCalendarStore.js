280f9338311cbb6c3e0f71b138f59041c997d913
Missa <missa_nieblas@hotmail.com>
2023-08-07 23:09:38 -0700

    Inicia Ceccion Eventos Calendario, Continuacion de Eventos CRUD y Crenado Nuevo Event Calendario

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,30 +1,35 @@
 
 import { useDispatch, useSelector } from 'react-redux'
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store/calendar/calendarSlice';
+import { calendarApi } from '../api';
 
 export const useCalendarStore = () => {
 
     const dispach = useDispatch();
     const { events, activeEvent } = useSelector( state => state.calendar);
+    const { user } = useSelector( state => state.auth);
 
     const SetActiveEvent = ( calendarEvent) => {
         dispach( onSetActiveEvent( calendarEvent ))
     }
 
     const startSavingEvent = async( calendarEvent ) => {
-        //TODO: llegar al backend
 
-        //Todo bien
+        //TODO: Update event
+
         if( calendarEvent._id ) {
             //Actualizando
             dispach( onUpdateEvent( {...calendarEvent} ) );
         } else {
             //Creando
-            dispach( onAddNewEvent( {...calendarEvent, _id: new Date().getTime() } ) )
+            const { data } = await calendarApi.post('/events', calendarEvent );
+            dispach( onAddNewEvent( {...calendarEvent, id: data.evento.id, user } ) )
         }
     }
 
     const startDeletingEvent = () => {
+        //Todo: Llegar al Backend
+
         dispach( onDeleteEvent() );
     }
 
